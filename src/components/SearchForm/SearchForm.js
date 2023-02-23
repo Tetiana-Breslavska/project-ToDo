3a1d2985d263d74f8baa30855d5f1a18cac372b4
Tetiana Breslavska <tvk.koba@gmail.com>
2023-02-23 04:19:19 +0100

    Add Container 11.5

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,7 +6,7 @@ const SearchForm = () => {
   return (
       <form className={styles.searchForm}>
         <TextInput placeholder="Search..." />
-        <Button text="search" />
+        <Button><span className="fa fa-search" /></Button> 
       </form>
   );
 };
