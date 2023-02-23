b65ba8b9e2040e463f6edab9d5100b419f0601d2
Tetiana Breslavska <tvk.koba@gmail.com>
2023-02-23 03:27:28 +0100

    Add icons

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,15 @@
+import styles from './SearchForm.module.scss';
+import TextInput from '../TextInput/TextInput.js';
+import Button from '../Button/Button.js';
+
+const SearchForm = () => {
+  return (
+      <form className={styles.searchForm}>
+        <TextInput placeholder="Search..." />
+        <Button text="search" />
+      </form>
+  );
+};
+
+
+export default SearchForm;
