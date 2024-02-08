7b8397f83913734a3899f825690c3393918b36fe
Tetiana-Breslavska <tvk.koba@gmail.com>
2024-02-08 16:59:03 +0200

    Rebuilt app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store'
@@ -8,7 +9,11 @@ import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
@@ -16,5 +21,5 @@ ReactDOM.render(
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.querySelector('#root')
+  // document.querySelector('#root')
 );
